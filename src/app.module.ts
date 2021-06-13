2f2f8a0592729af74732d568f20d586dedec1feb
Lucio <lucio@Lucios-MacBook-Pro.local>
2021-06-13 18:22:42 +0100

    Change path

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { ConfigModule } from '@nestjs/config';
     GraphQLModule.forRoot({
       autoSchemaFile: process.env.ENV === 'prod' ? true : join(process.cwd(), 'src/schema.gql'),
       playground: true,
+      introspection: true,
       debug: true,
     }),
     MongooseModule.forRootAsync({
